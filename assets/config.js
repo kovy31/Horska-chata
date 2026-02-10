0a0bea5e45ccc78a58c451edefb030d73cc7e6a7
kovy31 <vasekkovar31@gmail.com>
2026-02-10 01:21:59 +0100

    config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 // assets/config.js
 window.APP_CONFIG = {
-  OWNER: "TVUJ_GITHUB_LOGIN",
-  REPO: "NAZEV_REPA",
+  OWNER: "kovy31",
+  REPO: "Horska-chata",
   ISSUE_TITLE: "Booking data", // Issue, do kterého se ukládá JSON
   DEFAULT_TOTAL_CZK: 114938,
   KIDS_DISCOUNT: 0.75
